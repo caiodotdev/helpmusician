20080418ddedb4b5fbfa94a7b18fbf0c0a088819
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2022-04-07 12:48:27 -0300

    add drivemanager

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,12 +1,13 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BaseSeleniumTestCase(LiveServerTestCase):
     port = 8080
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.live_server_url)
         super(BaseSeleniumTestCase, self).setUp()
 

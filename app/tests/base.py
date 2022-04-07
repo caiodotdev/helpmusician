2a8b169019648259f90a60078a010e55ababa94f
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2022-04-07 14:17:40 -0300

    add chang

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,24 +1,24 @@
-import os
-
+from django.conf import settings
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from django.conf import settings
 
 BROWSERSTACK_LOCAL_IDENTIFIER = settings.BROWSERSTACK_LOCAL_IDENTIFIER
 BROWSERSTACK_USERNAME = settings.BROWSERSTACK_USERNAME
 BROWSERSTACK_ACCESSKEY = settings.BROWSERSTACK_ACCESSKEY
-DEBUG = settings.DEBUG
+LOCAL = False
 
 
 class BaseSeleniumTestCase(LiveServerTestCase):
     port = 8080
 
     def setUp(self):
-        if DEBUG:
+        if LOCAL:
+            print('----- euu')
             self.driver = webdriver.Chrome(ChromeDriverManager().install())
             self.driver.get(self.live_server_url)
         else:
+            print('----- eleee')
             caps = {
                 'browserstack.local': 'true',
                 'browserstack.localIdentifier': BROWSERSTACK_LOCAL_IDENTIFIER,
@@ -31,6 +31,7 @@ class BaseSeleniumTestCase(LiveServerTestCase):
             self.driver = webdriver.Remote(
                 command_executor='https://' + BROWSERSTACK_USERNAME + ":" + BROWSERSTACK_ACCESSKEY + '@hub-cloud.browserstack.com/wd/hub',
                 desired_capabilities=caps)
+            self.driver.get(self.live_server_url)
         super(BaseSeleniumTestCase, self).setUp()
 
     def tearDown(self) -> None:
